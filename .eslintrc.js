998169b32bca7e84406816075b68edd180dc8778
liqi.shi <liqi.shi@shopee.com>
2022-01-27 16:46:02 +0800

    feat: update eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,9 @@
 module.exports = {
-    extends: ["prettier"],
-    parser: '@typescript-eslint/parser',
-    env: {
-      es6: true,
-      browser: true
-    },
-    plugins: [
-      '@typescript-eslint',
-      "prettier",
-    ]
-  };
+  extends: ['plugin:prettier/recommended'],
+  parser: '@typescript-eslint/parser',
+  env: {
+    es6: true,
+    browser: true,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+};
