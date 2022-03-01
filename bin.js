e5ec765b2bf4bdb2a6710cde517f4a2450d00e5b
liqi.shi <liqi.shi@shopee.com>
2022-03-01 16:48:16 +0800

    feat: 降低 chalk 版本

--- bin.js
+++ bin.js
@@ -8,20 +8,7 @@ sade('i18n-shaking [entry]', true)
   .version(pkg.version)
   .describe('Size shaking for your i18n \r\n')
   .example('src/index.ts')
-  .action((entry, opts) => {
-    console.log(entry, opts);
-
-    console.log(i18nShaking);
-
-    const { handleResults, errors } = i18nShaking([entry]);
-
-    console.log('Results:' + '\r\n');
-    handleResults.forEach((r) => {
-      console.log(r) + '\r\n';
-    });
-    console.log('Error:' + '\r\n');
-    errors.forEach((r) => {
-      console.log(r) + '\r\n';
-    });
+  .action((entry) => {
+    i18nShaking([entry]);
   })
   .parse(process.argv, {});
