1af67c63c0dcd1806bd0dd16e62ef5758c145fb4
liqi.shi <liqi.shi@shopee.com>
2022-03-31 17:28:40 +0800

    feat: 调整文件

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,5 +1,5 @@
 // import parseI18nPointT from './parseI18nPointT';
-import parseI18nPointT from './parseI18nPointTPro';
+import parseI18nPointT from './parseI18n';
 
 const buildInPlugins = [parseI18nPointT];
 export default buildInPlugins;
